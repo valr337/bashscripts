7da8954fec2c09e0ed85697a6427be45b53c2ace
v <you@example.com>
2024-04-08 04:47:12 +0800

    Added and fixed script

--- handleExtract.sh
+++ handleExtract.sh
@@ -2,23 +2,20 @@
 #printf '%s\n' "${my_array[@]}"
 #getZipInfo=($( 7z l -ba Archive.7z | awk '/2024/{gsub("\.", "", $3); print $3}'))
 
-function extract() {
-	cmd="7z x "${$1}
-	echo $cmd
+# function extract() {
+# 	cmd="7z x "${$1}
+# 	echo $cmd
 	
-}
+# }
 
-cd "$HOME/Downloads" || exit
-pwd
+cd "$HOME/Downloads/testcmd" || exit
+#pwd
 
 commonpasswords=("cs.rin.ru" )
 ziptypes="zip,rar,7z,gzip,tar.gz,tgz,tar.bz2,tbz,tar.xz,txz,cbr"
 #ziptypes="zip,rar,7z"
-
-ziparr=("$(echo $ziptypes | tr "," " ")")
+IFS=',' read -r -a ziparr <<< "$ziptypes"
 #check arr contents
-#printf '%s\n' "${ziparr[@]}"
-
 cmd="find . -type f \( "
 for ((i = 0 ; i < ${#ziparr[@]} ; i++)); do
         cmd+="-iname \*.${ziparr[i]} -o "
@@ -30,10 +27,9 @@ cmd+=" \)"
 #exec $cmd
 #cmd="find \. -type f \( -iname \*.zip -o -iname \*.rar \)"
 x=$(eval "$cmd")
-filesarr=("$(echo $x | tr " " ",")")
+IFS=',' read -r -a filesarr <<< "$x"
 
-printf '%s\n' "${filesarr[@]}"
-echo "$x"
+#printf '%s\n' "${filesarr[@]}"
 
 tmp=".tmp"
 #make dir if it does not exist
@@ -43,33 +39,36 @@ if [ ! -d "$tmp" ]; then
 fi
 
 for ((i = 0 ; i < ${#filesarr[@]} ; i++));do
-	path="${tmp}"
-	condend=false
+	path=${tmp}
+	conend=false
 	echo "${filesarr[i]}"
-	7z x -y "${filesarr[i]}" -o${tmp}
+	7z x -y "${filesarr[i]}" -o${tmp} > nul
 	#still have to check for errors when extracting archive
 
 	#first basic check for common zip archives
 	#repeat until theres no more directories
-	while [ ! $condend ]
+	echo "path ${tmp}"
+	while [ $conend = false ]
 	do
-		echo "${path}/*"
+		#echo "next: ${path}"/*
 		#why /* instead of / ? check +1 so we dont need to get curr dir when we want to use mv
-		if [ "$(find "${path}/*" -maxdepth 1 -type d -printf 1 | wc -m)" -eq 2 \
-			-a "$(find "${path}/*" -maxdepth 1 ! -type d -printf 1 | wc -m)" -eq 0 ]; then
-			path+="/"
+		if [ "$(find ${path} -maxdepth 1 -type d -printf 1 | wc -m)" -eq 2 \
+			-a "$(find ${path} -maxdepth 1 ! -type d -printf 1 | wc -m)" -eq 0 ]; then
+			path+="/*"
+			echo path ${path}
 		# It has only one subdirectory and no other content
 		else
-			echo $path
-			condend=true
+			conend=true
+			echo "X"
 		fi		
 	done
+	echo "after" ${path}
 	#passed all empty dirs
 	#keep record of original zip name in text file
 
 	#check whether zip is a steam game
-	foldercontents="${path}/*"
-	echo "$foldercontents"
+	foldercontents=${path}/*
+	echo "foldercontents: " $foldercontents
 
 	if [ "${foldercontents}" == "depotcache" ] && [ "${foldercontents}" == "steamapps" ]; then
 
@@ -85,14 +84,16 @@ for ((i = 0 ; i < ${#filesarr[@]} ; i++));do
 
 	if [ $path == $tmp ]; then
 		#if path is empty that means that folder is already pruned
-		mkdir "${filesarr[i]}"
-		mv "$tmp/*" "${filesarr[i]}"
+		echo "path empty"
+		echo "${filesarr[i]}"
+		mkdir "${filesarr[i]%.*}"
+		mv "${tmp}"/* "${filesarr[i]%.*}"
 	else	
 		if [ "$steampath" != "" ]; then
 			path=$steampath
 			steampath=""
 		fi 	
-		mv "${path}" "$HOME/Downloads"
+		mv ${path} "$HOME/Downloads/testcmd"
 	fi
 
 	#clear tmp dir
