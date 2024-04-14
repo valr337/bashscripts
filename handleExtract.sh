41873cc3a0bfc38e1c74682cd1f36a1ae8a657f1
yx <you@example.com>
2024-04-14 21:57:47 +0800

    (not working) version before major changes to glob

--- handleExtract.sh
+++ handleExtract.sh
@@ -9,7 +9,8 @@
 # }
 
 cd "$HOME/Downloads/test/testcmd" || exit
-#pwd
+#tree -L 1
+pwd
 
 commonpasswords=("cs.rin.ru" "hello")
 ziptypes="zip,rar,7z,gzip,tar.gz,tgz,tar.bz2,tbz,tar.xz,txz,cbr"
@@ -27,7 +28,14 @@ cmd+=" \)"
 #exec $cmd
 #cmd="find \. -type f \( -iname \*.zip -o -iname \*.rar \)"
 x=$(eval "$cmd")
-IFS=',' read -r -a filesarr <<< "$x"
+#echo "${x}"
+
+set -o noglob         # See special Note, below.
+IFS=$'\n' filesarr=($x)
+set +o noglob         # See special Note, below.
+
+#IFS=$'\n' read -r -a filesarr <<< "$x"
+#echo "array count: ${#filesarr[@]}"
 
 #printf '%s\n' "${filesarr[@]}"
 
@@ -37,29 +45,32 @@ if [ ! -d "$tmp" ]; then
 	mkdir $tmp
 fi
 
+#echo "${filesarr[@]}"
+
 for ((i = 0 ; i < ${#filesarr[@]} ; i++));do
 	path=${tmp}
 	conend=false
-	echo "${filesarr[i]}"
+	echo "$i: ${filesarr[i]}"
 
 	#error checking
-	x="$(7z t -p "${filesarr[i]}" 2>&1 >/dev/null)"
+	x="$(7z -bso0 -bsp0 -p t "${filesarr[i]}" 2>&1)"
 	y="$?"
-	echo "error code $?"
+	echo "error code $y"
 
 	if [ "$y" == 0 ]; then
-		echo "ok"
+		#echo "ok"
 		#OK to extract, no archive error or password
-		7z x -y "${filesarr[i]}" -o${tmp} 2>/dev/null	
+		7z -bso0 -y x  "${filesarr[i]}" -o${tmp}
 
 	elif [ "$y" == 2 ]; then
-		echo "no"
-		if [[ "$x" == *"Wrong password"* ]]; then
+		echo "$x"
+		if [[ "$x" =~ "Wrong password?" ]]; then
 			#password protected
 			for ((j = 0; j < ${#commonpasswords[@]} ; j++)); do
-				echo "j: $j"
-				if [ "$(7z t -p"${commonpasswords[j]}" "${filesarr[i]}" > NUL: 2>&1)" ]; then
-					7z x -y -p"${commonpasswords[j]}" "${filesarr[i]}" -o${tmp} 2>/dev/null	
+				#echo "j: ${commonpasswords[j]}"
+				if [ "$(7z -bso0 -p"${commonpasswords[j]}" t "${filesarr[i]}")" ]; then
+					7z -bso0 -y -p"${commonpasswords[j]}" x "${filesarr[i]}" -o${tmp}	
+					#echo ${tmp}/*
 				fi
 			done
 		else
@@ -75,57 +86,83 @@ for ((i = 0 ; i < ${#filesarr[@]} ; i++));do
 
 	#first basic check for common zip archives
 	#repeat until theres no more directories
-	echo "path ${tmp}"
+	#echo "path ${tmp}"
 	while [ $conend = false ]
 	do
-		#echo "next: ${path}"/*
-		#why /* instead of / ? check +1 so we dont need to get curr dir when we want to use mv
 		if [ "$(find ${path} -maxdepth 1 -type d -printf 1 | wc -m)" -eq 2 \
 			-a "$(find ${path} -maxdepth 1 ! -type d -printf 1 | wc -m)" -eq 0 ]; then
 			path+="/*"
-			echo path ${path}
-		# It has only one subdirectory and no other content
+			#echo path ${path}
+			# It has only one subdirectory and no other content
 		else
+			#echo ${path}
 			conend=true
-			echo "X"
+			#echo "X"
 		fi		
 	done
-	echo "after" ${path}
+	#echo "after" ${path}
 	#passed all empty dirs
 	#keep record of original zip name in text file
 
 	#check whether zip is a steam game
-	foldercontents=${path}/*
-	echo "foldercontents: " $foldercontents
+	
+	foldercontents=(${path}/*)
+	#echo "foldercontents: " "${foldercontents[@]}"
 
-	if [ "${foldercontents}" == "depotcache" ] && [ "${foldercontents}" == "steamapps" ]; then
+	# if [[ " ${array[*]} " =~ [[:space:]]${value}[[:space:]] ]]; then
+    # # whatever you want to do when array contains value
+	# fi
+
+	if [[ "${foldercontents[*]}" =~ "depotcache" ]] && [[ "${foldercontents[*]}" =~ "steamapps" ]]; then
 
 		#its a steam game archive
-		steampath="${path}/steamapps/common/"
-		for f in ${steampath}; do
-			echo f
+		steampath=${path}
+		steampath+="/steamapps/common/"
+		for f in ${steampath}/*; do
+#			echo $f
 			if [ "$f" != "Steamworks Shared" ]; then
-				steampath+="/${f}"
+				steampath=${f}
 			fi
 		done
+		#echo ${steampath}
 	fi
+	
+	#echo ${path}
+
+
 
 	if [ $path == $tmp ]; then
 		#if path is empty that means that folder is already pruned
-		echo "path empty"
-		echo "${filesarr[i]}"
-		mkdir "${filesarr[i]%.*}"
-		mv "${tmp}"/* "${filesarr[i]%.*}"
+		#echo "path empty"
+		#echo "${filesarr[i]}"
+		fname="${filesarr[i]:2}" #remove ./
+		fname="${fname%.*}" #remove extension
+		#echo "${fname}"
+		if [ ! -d "output/${fname}" ]; then
+			mkdir "output/${fname}"
+			mv "${tmp}"/* "output/${fname}"
+		else
+			echo "output/${fname} exists"
+		fi
+
 	else	
 		if [ "$steampath" != "" ]; then
 			path=$steampath
 			steampath=""
 		fi 	
-		mv ${path} "$HOME/Downloads/testcmd"
+
+		tmpsize=${#tmp}
+		fpath=output/${path:tmpsize}
+		echo $fpath
+		if [ ! -d "${fpath}" ]; then
+			mv ${fpath} "$HOME/Downloads/test/testcmd/output"
+		else
+			echo ${fpath} "exists"
+		fi
 	fi
 
 	#clear tmp dir
-	rm -rf "{$tmp}/*"
+	rm -rf ${tmp}/*
 
 
 	#still have to check for errors
